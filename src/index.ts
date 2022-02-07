94644d25bad82627295b8bf75bc293d9a86a9310
whamer100 <whammer100@aol.com>
2022-02-07 01:24:47 -0800

    eslint and some todo stuff

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
 console.log("Hello, World!")
+
+// TODO LIST OF THINGS TO DO
+// TODO: get bot framework started
+//       - dotenv
+//       - configure eslint
+//       - disable typoes <- that
+//       - this shit: E:\Random Project Things\programming things\bandori-discord-bot\src
+// TODO: start working on game handler
+//       - Game class
+//       - GamePlayer class + registering players
+//       - GameEvent, GameState, etc etc whatever just get it working 4Head
+// TODO: make a basic game example (click the button to win!)
+// TODO: click the button to win 2 player edition
+// TODO: make connect four
+// TODO: ???
+// TODO: profit
